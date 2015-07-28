584326aec99f331ff5c3cd896f0281f280c940b8
Thiago Pagonha <thi.pag@gmail.com>
2015-07-28 20:04:56 -0300

    Creating interface to comunicate with my rest service

--- app/src/main/java/com/planyourexchange/rest/api/ServerApi.java
+++ app/src/main/java/com/planyourexchange/rest/api/ServerApi.java
@@ -4,12 +4,20 @@ import com.planyourexchange.rest.model.Country;
 
 import java.util.List;
 
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
 import retrofit.http.GET;
+import retrofit.http.POST;
 
 /**
  * Created by thiago on 28/07/15.
  */
 public interface ServerApi {
+
+    @FormUrlEncoded
+    @POST("/token-auth/")
+    String login(@Field("username") String userName, @Field("password") String password);
+
     @GET("/cities/")
     List<Country> listCountries();
 }
--- app/src/main/java/com/planyourexchange/rest/service/ServerService.java
+++ app/src/main/java/com/planyourexchange/rest/service/ServerService.java
@@ -1,7 +1,39 @@
 package com.planyourexchange.rest.service;
 
+import com.planyourexchange.PropertyReader;
+import com.planyourexchange.rest.api.ServerApi;
+
+import retrofit.RequestInterceptor;
+import retrofit.RestAdapter;
+
 /**
  * Created by thiago on 28/07/15.
  */
 public class ServerService {
+
+    private ServerApi serverApi;
+    private String authToken;
+
+    public ServerService(String serviceUrl, String userName, String password) {
+
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .setEndpoint(serviceUrl)
+                .setRequestInterceptor(new RequestInterceptor() {
+                    @Override
+                    public void intercept(RequestFacade request) {
+                        if (authToken != null) {
+                            request.addHeader("Authorization", authToken);
+                        }
+                    }
+                }).build();
+
+        this.serverApi = restAdapter.create(ServerApi.class);
+
+        this.authToken = this.serverApi.login(userName,password);
+    }
+
+    public ServerApi getServerApi() {
+        return serverApi;
+    }
 }
