ec9217c4bc85b226cec2f404caaa4d3a8d756cde
Thiago Pagonha <thi.pag@gmail.com>
2015-08-02 23:58:23 -0300

    Compilation error, extending interface

--- app/src/main/java/com/planyourexchange/tasks/RestLoaderTask.java
+++ app/src/main/java/com/planyourexchange/tasks/RestLoaderTask.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.view.ViewGroup;
 
 import com.planyourexchange.app.PlanYourExchangeContext;
+import com.planyourexchange.rest.model.BaseModel;
 import com.planyourexchange.rest.model.City;
 import com.planyourexchange.rest.model.Country;
 import com.planyourexchange.views.ModelView;
@@ -14,7 +15,7 @@ import java.util.List;
 /**
  * Created by thiago on 01/08/15.
  */
-public class RestLoaderTask<Key,Model> extends AsyncTask<Key, Void, List<Model>> {
+public class RestLoaderTask<Key,Model extends BaseModel> extends AsyncTask<Key, Void, List<Model>> {
 
     private Context context;
     private ViewGroup viewGroup;
