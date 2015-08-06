0b9ccca163c9f6911e1ee706faa5280082a52305
Thiago Pagonha <thi.pag@gmail.com>
2015-08-06 13:43:48 -0300

    Fixing compilation errors

--- app/src/main/java/com/planyourexchange/fragments/base/BaseFragment.java
+++ app/src/main/java/com/planyourexchange/fragments/base/BaseFragment.java
@@ -1,16 +1,15 @@
 package com.planyourexchange.fragments.base;
 
-import android.app.Fragment;
+
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
